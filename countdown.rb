c2ceb93d4fa7fcfb1dcb5c22861795578fb013b4
Yuriko <ygabriela.arnott@gmail.com>
2022-11-23 11:28:20 -0500

    solved

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
-
-def countdown
+require 'pry'
+def countdown(number)
+    while number > 0 
+        puts "#{number} SECOND(S)!"    
+       number -= 1
+    end   
+    "HAPPY NEW YEAR!"
+end
+binding pry
+def countdown_with_sleep
 end
