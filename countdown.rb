51fb11b8f9ea95bafbcf615a0aa75957b5072be0
Yuriko <ygabriela.arnott@gmail.com>
2022-11-23 11:37:10 -0500

    update

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,5 @@
 #write your code here
-require 'pry'
+#equire 'pry'
 def countdown(number)
     while number > 0 
         puts "#{number} SECOND(S)!"    
@@ -8,5 +8,12 @@ def countdown(number)
     "HAPPY NEW YEAR!"
 end
 binding pry
-def countdown_with_sleep
+def countdown_with_sleep(number)
+    while number > 0 
+        sleep 1 and puts "#{number} SECOND(S)!" 
+         
+       number -= 1
+    end   
+    "HAPPY NEW YEAR!"
 end
+#binding pry
